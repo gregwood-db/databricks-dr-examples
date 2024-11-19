7d4f3c25a8ab4db79d8d09fa4c1120b4fde754fd
gregwood-db <42991536+gregwood-db@users.noreply.github.com>
2024-11-19 12:00:37 -0500

    Update sync_creds_and_locs.py

--- sync_creds_and_locs.py
+++ sync_creds_and_locs.py
@@ -3,8 +3,11 @@
 # This is a minimal script to sync storage credentials and external locations between two workspaces.
 # To run it, you must first fill out the appropriate mapping files:
 #   - <cloud>_cred_mapping.csv: contains the required mapping parameters to translate a credential between two regions.
-#     Note that this varies by cloud. For AWS, this is simply the IAM role ARN. For Azure, you will need the SP and
-#     Managed Identity info.
+#     Note that this varies by cloud. For AWS, this is simply the IAM role ARN. For Azure, you will need the SP OR
+#     Managed Identity info. Note that for Azure, only ONE of the following should be provided:
+#       + target_mgd_id_connector (if using a standard access connector)
+#       + target_mgd_id_identity (if using a user-assigned identity)
+#       + target_sp fields (if using a Service Principal)
 #   - ext_location_mapping.csv: maps the target URL between source and target. For AWS, also has an option to provide
 #     an S3 access point for the target region.
 #
@@ -25,13 +28,13 @@ from databricks.sdk.service import catalog
 import pandas as pd
 
 # inputs
-cloud_type = "aws"
-cred_mapping_file = "data/aws_cred_mapping.csv"
+cloud_type = "azure"
+cred_mapping_file = "data/azure_cred_mapping.csv"
 loc_mapping_file = "data/ext_location_mapping.csv"
-source_host = "https://<my-source-workspace>.cloud.databricks.com/"
-source_pat = "<source_workspace_PAT>"
-target_host = "https://<my-target-workspace>.cloud.databricks.com/"
-target_pat = "<target_workspace_PAT>"
+source_host = "<primary_workspace_hostname>"
+source_pat = "<primary_workspace_PAT>"
+target_host = "<secondary_workspace_hostname>"
+target_pat = "<secondary_workspace_PAT>"
 
 # create WorkspaceClient objects
 w_source = WorkspaceClient(host=source_host, token=source_pat)
@@ -59,6 +62,7 @@ for cred in creds_to_create:
     cred_name = cred.name
     cred_read_only = cred.read_only
     cred_comment = cred.comment
+    print(f"Creating storage credential {cred_name}...")
 
     if cloud_type == "aws":
         # get cred IAM role based off of name
@@ -74,7 +78,7 @@ for cred in creds_to_create:
                                             read_only=cred_read_only,
                                             comment=cred_comment,
                                             aws_iam_role=cred_iam_role)
-        elif cloud_type == "azure":
+    elif cloud_type == "azure":
         # get SP and Mgd ID info based off of name
         try:
             managed_id_connector = \
@@ -90,25 +94,38 @@ for cred in creds_to_create:
 
         # create storage credential in target WS
         if managed_id_connector:
-            cred_mgd_id = catalog.AzureManagedIdentityRequest(access_connector_id=managed_id_connector,
-                                                              managed_identity_id=managed_id_identity)
+            cred_mgd_id = catalog.AzureManagedIdentityRequest(access_connector_id=managed_id_connector)
+            w_target.storage_credentials.create(name=cred_name,
+                                                read_only=cred_read_only,
+                                                comment=cred_comment,
+                                                azure_managed_identity=cred_mgd_id)
+        elif managed_id_identity:
+            cred_mgd_id = catalog.AzureManagedIdentityRequest(access_connector_id=managed_id_identity)
             w_target.storage_credentials.create(name=cred_name,
                                                 read_only=cred_read_only,
                                                 comment=cred_comment,
                                                 azure_managed_identity=cred_mgd_id)
         else:
-            cred_sp = catalog.AzureServicePrincipal(directory_id=sp_directory,
-                                                    application_id=sp_appid,
-                                                    client_secret=sp_secret)
-            w_target.storage_credentials.create(name=cred_name,
-                                                read_only=cred_read_only,
-                                                comment=cred_comment,
-                                                azure_service_principal=cred_sp)
-
+            try:
+                cred_sp = catalog.AzureServicePrincipal(directory_id=sp_directory,
+                                                        application_id=sp_appid,
+                                                        client_secret=sp_secret)
+                w_target.storage_credentials.create(name=cred_name,
+                                                    read_only=cred_read_only,
+                                                    comment=cred_comment,
+                                                    azure_service_principal=cred_sp)
+            except Exception:
+                print(f"Could not create credential {cred_name}. Please make sure that only one of \
+                managed_id_connector, managed_id_identity or service_principal info is provided in the mapping.")
+                
     elif cloud_type == "gcp":
         print("GCP not yet implemented.")
+        continue
     else:
         print("Cloud type must be one of AWS, GCP, or Azure.")
+        continue
+
+    print(f"Created storage credential {cred_name}.")
 
 # compare source and target external locations
 # we can only do this by name since the URL and IDs will change between workspaces
@@ -128,6 +145,7 @@ for loc in locs_to_create:
     loc_comment = loc.comment
     loc_fallback = loc.fallback
     loc_read_only = loc.read_only
+    print(f"Creating external location {loc_name}...")
 
     if cloud_type == "aws":
         try:
@@ -155,7 +173,7 @@ for loc in locs_to_create:
     elif cloud_type == "azure":
         try:
             url = loc_df['target_url'].loc[loc_df['source_loc_name'] == loc_name].iloc[0]
-        except KeyError:
+        except (KeyError, IndexError):
             print(f"Could not create location {loc_name}. Please check mapping file.")
             continue
 
@@ -167,5 +185,9 @@ for loc in locs_to_create:
                                            url=url)
     elif cloud_type == "gcp":
         print("GCP not yet implemented.")
+        continue
     else:
         print("Cloud type must be one of AWS, GCP, or Azure.")
+        continue
+
+    print(f"External location {loc_name} created.")
